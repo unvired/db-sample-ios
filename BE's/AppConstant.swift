48d98c940c6d24964d17d82a1b5de6293cea97d2
Pavithra <pavithra.vp@unvired.io>
2020-04-24 12:49:07 +0530

    updated new metadata and PA functions

--- BE's/AppConstant.swift
+++ BE's/AppConstant.swift
@@ -3,14 +3,14 @@ import UIKit
 class AppConstants {
 
 	// App Name
-	static let APP_NAME = "UnviredDBSample"
+	static let APP_NAME = "UNVIRED_DB_SAMPLE"
 	
 	// BE Names
 	static let BE_CONTACT = "CONTACT";
 
 	
 	// PA Function Names
-	static let PA_CREATE_CONTACT = "UNVIREDDBSAMPLE_PA_CREATE_CONTACT"; // Create Contact
-	static let PA_GET_CONTACT = "UNVIREDDBSAMPLE_PA_GET_CONTACT"; // Get Contact
+	static let PA_CREATE_CONTACT = "UNVIRED_DB_SAMPLE_PA_CREATE_CONTACT"; // Create Contact
+	static let PA_GET_CONTACT = "UNVIRED_DB_SAMPLE_PA_GET_CONTACT"; // Get Contact
 
 }
